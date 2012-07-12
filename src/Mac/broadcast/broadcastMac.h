47f58fd2966ab696b795dca47c5db2e4e8317464
Marcin Szczodrak <msz@cs.columbia.edu>
2012-07-12 00:12:01 -0400

    add broadcast mac

---
+++ src/Mac/broadcast/broadcastMac.h
@@ -0,0 +1,43 @@
+/*
+ *  Broadcast mac module for Fennec Fox platform.
+ *
+ *  Copyright (C) 2010-2012 Marcin Szczodrak
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
+ * Network: Broadcast Mac Protocol
+ * Author: Marcin Szczodrak
+ * Date: 8/20/2010
+ * Last Modified: 1/5/2012
+ */
+
+#ifndef __broadcast_MAC_H__
+#define __broadcast_MAC_H__
+
+#define MAX_NEIGHBORHOOD_SIZE	12
+#define TX_PERIOD		2000
+#define REPORT_PERIOD		3
+
+nx_struct broadcast_header {
+  nx_uint16_t src;
+};
+
+nx_struct broadcast_mac_footer {
+  nx_uint16_t footer;
+};
+
+#endif
