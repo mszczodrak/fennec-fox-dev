fa5a826f687ecd984df7bc746cabbe266d1922d1
Marcin Szczodrak <msz@cs.columbia.edu>
2012-12-16 21:09:12 -0500

    tmp

--- src/Fennec/ff_structs.h
+++ src/Fennec/ff_structs.h
@@ -59,6 +59,56 @@ typedef nx_struct fennec_header_t {
 } fennec_header_t;
 
 
+#include <Serial.h>
+
+typedef union message_header {
+  cc2420_header_t cc2420;
+  serial_header_t serial;
+} message_header_t;
+
+typedef union TOSRadioFooter {
+  cc2420_footer_t cc2420;
+} message_footer_t;
+
+
+
+
+typedef nx_struct metadata_t {
+  nx_uint8_t rssi;
+  nx_uint8_t lqi;
+  nx_uint8_t tx_power;
+#ifndef TOSSIM
+  nx_bool crc;
+  nx_bool ack;
+  nx_bool timesync;
+#else
+  nx_uint8_t crc;
+  nx_uint8_t ack;
+  nx_uint8_t timesync;
+#endif
+  nx_uint32_t timestamp;
+  nx_uint16_t rxInterval;
+  nx_uint16_t maxRetries;
+  nx_uint16_t retryDelay;
+} metadata_t;
+
+
+typedef nx_struct message_t {
+  nx_uint8_t header[sizeof(message_header_t)];
+  nx_uint8_t data[TOSH_DATA_LENGTH];
+  nx_uint8_t footer[sizeof(message_footer_t)];
+  nx_uint8_t metadata[sizeof(metadata_t)];
+  nx_uint16_t conf;
+  nx_uint8_t rssi;
+  nx_uint8_t lqi;
+  nx_uint8_t crc;
+  nx_uint8_t ack;
+  nx_uint16_t rxInterval;
+} message_t;
+
+
+
+
 typedef nx_struct msg_t {
 	nx_uint8_t data[128];
 	nx_uint8_t len;
