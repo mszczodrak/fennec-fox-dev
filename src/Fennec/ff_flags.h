939cfc6dbbc5d166438bcc3a60abd1c2cc11203e
Marcin Szczodrak <msz@cs.columbia.edu>
2013-02-14 20:17:55 -0500

    update FF for new compiler version

--- src/Fennec/ff_flags.h
+++ src/Fennec/ff_flags.h
@@ -1,7 +1,7 @@
 /*
  *  Fennec Fox platform.
  *
- *  Copyright (C) 2010-2012 Marcin Szczodrak
+ *  Copyright (C) 2010-2013 Marcin Szczodrak
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
