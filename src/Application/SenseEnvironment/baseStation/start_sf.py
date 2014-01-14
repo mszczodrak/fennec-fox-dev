881cace9316c00a95fb0884e4998a94ff593c684
Marcin K Szczodrak <marcin@ieee.org>
2014-01-14 00:00:13 -0500

    lincese

--- src/Application/SenseEnvironment/baseStation/start_sf.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import os
-
-os.system("./sf/sf 9002 /dev/ttyUSB0 115200")
--- src/Application/SenseEnvironment/baseStation/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm EnvSerialMsg.py*
-cd ./sf
-make clean
-rm Makefile Makefile.in aclocal.m4 autoconf.h autoconf.h.in config.log config.status 
-rm configure serialpacket.c serialpacket.h serialprotocol.h stamp-h1
-rm -rf autom4te.cache config-aux
