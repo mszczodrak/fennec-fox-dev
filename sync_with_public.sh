039f747c162f60227adb11bee284e8644090915f
Marcin Szczodrak <msz@cs.columbia.edu>
2014-01-18 16:15:57 -0500

    sync

--- sync_with_public.sh
+++ sync_with_public.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # Marcin Szczodrak
+# This program syncs fennec-fox-dev repo with the public one
 
-DEV=./
-PUB=../fennec-fox
-MAIN_FOLDER=src
-PUBS=pubs
+DEV_REPO=`pwd`
+PUB_REPO=`pwd`/../fennec-fox
+PUB_CODE=pubs
 
-echo "Delete pub repo files"
-rm -rf $PUB/$MAIN_FOLDER
+echo "Clean Repo"
+fennec clean
+
+echo "Delete all files from $PUB_REPO"
+rm -rf $PUB_REPO/*
 
 while read -r path
 do
 	echo "Copying - $path"
-	cp -R  --parents $path $PUB
-done < "$PUBS"
+	cp -R  --parents $path $PUB_REPO
+done < "$PUB_CODE"
 
-cd $PUB
-#git add *
+cd $PUB_REPO
+git add *
+git commit -am "Sync from `date`"
+git push
 
