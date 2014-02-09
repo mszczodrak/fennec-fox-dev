96047cfb830b2f39deb129f9e09d35812e6bc42a
Marcin Szczodrak <msz@cs.columbia.edu>
2014-02-08 19:01:58 -0500

    start ReadSensor

--- src/Application/create.sh
+++ src/Application/create.sh
@@ -2,22 +2,20 @@
 # Author: Marcin Szczodrak
 # msz@cs.columbia.edu
 
-BASE_EXAMPLE="null"
+BASE_EXAMPLE="nullApp"
 NEW_MODULE=$1
 
 PP="_"
 EXPECTED_ARGS=1
-LAYER="App"
-FULL_LAYER_NAME="application"
 C="C.nc"
 P="P.nc"
 H=".h"
-OLD_C=$BASE_EXAMPLE$LAYER$C
-OLD_P=$BASE_EXAMPLE$LAYER$P
-OLD_H=$BASE_EXAMPLE$LAYER$H
-NEW_C=$NEW_MODULE$LAYER$C
-NEW_P=$NEW_MODULE$LAYER$P
-NEW_H=$NEW_MODULE$LAYER$H
+OLD_C=$BASE_EXAMPLE$C
+OLD_P=$BASE_EXAMPLE$P
+OLD_H=$BASE_EXAMPLE$H
+NEW_C=$NEW_MODULE$C
+NEW_P=$NEW_MODULE$P
+NEW_H=$NEW_MODULE$H
 
 if [ $# -ne $EXPECTED_ARGS ]; then
 	echo "Usage" $0 "<new_module_name>";
@@ -29,9 +27,9 @@ cp -R $BASE_EXAMPLE $NEW_MODULE
 cd $NEW_MODULE
 
 
-sed "s/null/$NEW_MODULE/g" $OLD_C > $NEW_C
-sed "s/null/$NEW_MODULE/g" $OLD_P > $NEW_P
-sed "s/null/$NEW_MODULE/g" $OLD_H > $NEW_H
+sed "s/$BASE_EXAMPLE/$NEW_MODULE/g" $OLD_C > $NEW_C
+sed "s/$BASE_EXAMPLE/$NEW_MODULE/g" $OLD_P > $NEW_P
+sed "s/$BASE_EXAMPLE/$NEW_MODULE/g" $OLD_H > $NEW_H
 
 
 rm $BASE_EXAMPLE*
@@ -45,7 +43,7 @@ echo -n $FENNEC_FOX_LIB
 echo "/src/support/sfc/fennec.sfl"
 echo
 echo -n "use "
-echo -n $FULL_LAYER_NAME
+echo -n " <layer>"
 echo -n " <give_it_a_name> "
 echo -n $PWD/
 echo $NEW_MODULE
