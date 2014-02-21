472e10db2f03f26eb42625348068d94975a12508
Marcin Szczodrak <marcin@ieee.org>
2014-02-20 19:49:10 -0500

    update sim_sensor_input.c

--- src/support/cape/src/sim_sensor_input.c
+++ src/support/cape/src/sim_sensor_input.c
@@ -49,6 +49,7 @@
 
 #include "sim_sensor_input.h"
 #include "sim_tossim.h"
+#include "sim_io.h"
 #include "sensor_input_pkt.h"
 
 struct sim_sensor_client_list *sim_sensor_clients;
@@ -200,16 +201,14 @@ void sim_sensor_open_socket(int port) {
 
 void sim_sensor_forward_packet(const void *packet, const int len) {
 	struct sensor_input_pkt *pkt = (struct sensor_input_pkt *)packet;
-	printf("receive a packet\n");
-	printf("rec node: %d\n", ntohs(pkt->node_id));
-	printf("rec sensor: %d\n", ntohs(pkt->sensor_id));
-	printf("rec val: %d\n", ntohl(pkt->value));
-	
-//	free(pkt);
+	//printf("receive a packet\n");
+	//printf("rec node: %d\n", ntohs(pkt->node_id));
+	//printf("rec sensor: %d\n", ntohs(pkt->sensor_id));
+	//printf("rec val: %d\n", ntohl(pkt->value));
+
+	sim_outside_write_input(ntohs(pkt->node_id), ntohl(pkt->value), 
+						ntohs(pkt->sensor_id), 0);
 }
-
-
-
 
 
 void sim_sensor_process() {
