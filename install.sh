b5eb8937287e7f56dec86f1137aaf5a4ddd953b8
Marcin Szczodrak <msz@cs.columbia.edu>
2014-02-27 13:36:09 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
-# Marcin Szczodrak
+#
+# Copyright (c) 2009 Columbia University. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# - Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the
+#   distribution.
+# - Neither the name of the copyright holder nor the names of
+#   its contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Bash script to set FENNEC_FOX_LIB system environment variable to the
+# location where Fennec Fox is stored.
+#
+# @author	Marcin Szczodrak
+# @updated	February 27 2014
 
-sed -i '/FENNEC_FOX_LIB/ d' ~/.profile
-echo "export FENNEC_FOX_LIB=`pwd`/src" >> ~/.profile
-echo "Please logout or at least enter 'source ~/.profile'"
+PROFILE_FILE=~/.profile
+
+if [ -f $PROFILE_FILE ];
+then
+	sed -i '/FENNEC_FOX_LIB/ d' $PROFILE_FILE
+	echo "export FENNEC_FOX_LIB=`pwd`/src" >> $PROFILE_FILE
+	echo "Installation Succesful"
+	echo "Please logout or at least enter 'source $PROFILE_FILE'"
+else
+	echo "Trying to set \$FENNEC_FOX_LIB in $PROFILE_FILE file"
+	echo "$PROFILE_FILE file does not exist"
+	echo "Installation Aborted"
+fi
 
 
