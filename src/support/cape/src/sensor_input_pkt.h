0688c20ab75d30fb4be849bf3da540904a647ad8
Marcin Szczodrak <marcin@ieee.org>
2014-03-05 18:43:09 -0500

    up

--- src/support/cape/src/sensor_input_pkt.h
+++ src/support/cape/src/sensor_input_pkt.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 
 struct sensor_input_pkt {
-	uint16_t node_id;
-	uint16_t sensor_id;
-	uint32_t value;	
+	uint16_t node_id;	/* Mote address ID */
+	uint16_t sensor_id;	/* Sensor number on mote with address node_id */
+	uint32_t value;		/* Sensor measurement value */
 };
 
 #endif
