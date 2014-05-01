65406fc13535018cd84f73528367c68e41a6ca17
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-05-01 11:46:12 -0400

    submit examples like cc2420_lpl0_r72_d60_p03_ack_cong

--- src/am/cc2420/cc2420.h
+++ src/am/cc2420/cc2420.h
@@ -1,7 +1,7 @@
 #ifndef __cc2420_H_
 #define __cc2420_H_
 
-#if defined(PLATFORM_Z1) || defined(TOSSIM)
+#if !defined(CC2420X) || defined(PLATFORM_Z1) || defined(TOSSIM)
 typedef TMicro TRadio;
 typedef uint16_t tradio_size;
 #else
