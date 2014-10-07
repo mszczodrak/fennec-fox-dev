845285ba7283df7fc1ba54d493bd0e73ee22d180
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-10-06 20:02:11 -0400

    up

--- src/Fennec/ff_sfc.h
+++ src/Fennec/ff_sfc.h
@@ -3,14 +3,14 @@
 
 #include "global_data_values.h"
 #include "local_data_values.h"
+#include "cache_data_values.h"
 #include "ff_caches.h"
 #include "ff_modules.h"
 #include "ff_processes.h"
 #include "ff_states.h"
-#include "global_data.h"
 #include "global_data_msg.h"
-#include "local_data.h"
 #include "variable_constants.h"
 #include "variable_lookup.h"
+
 
 #endif
